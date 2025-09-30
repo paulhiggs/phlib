4fa915b4eafc86251b420ae4acdf02f50da285b2
Paul Higgs <paul_higgs@hotmail.com>
2025-09-30 06:35:03 +0100

    cleanup and use definded types in header

--- phlib.d.ts
+++ phlib.d.ts
@@ -4,24 +4,24 @@
  * @param {String} requiredType  the desired tyoe
  * @returns {boolean or string} the type of the argument or a boolean if the type matches the requiredType
  */
-export function datatypeIs(arg: any, requiredType?: string): boolean;
-export function quote(str: any): any;
-export function elementize(str: any): any;
-export function attribute(attr: any, elem?: string): any;
-export function celsiusToFahrenheit(celsius: any): number;
-export function fahrenheitToCelsius(fahrenheit: any): number;
+export function datatypeIs(arg: any, requiredType?: string): boolean | string;
+export function quote(str: string): string;
+export function elementize(str: string): string;
+export function attribute(attr: string, elem?: string): string;
+export function celsiusToFahrenheit(celsius: number): number;
+export function fahrenheitToCelsius(fahrenheit: number): number;
 export function average(...args: any[]): number;
-export function toFixed(n: any, fixed: any): number;
+export function toFixed(n: number, fixed: number): number;
 export function randomNumberInRange(min: number, max: number): number;
 export function randomBoolean(): boolean;
 export function coinToss(): string;
 export function isWeekday(date: Date): boolean;
-export function reverse(str: any): any;
-export function isEven(num: any): boolean;
+export function reverse(str: string): string;
+export function isEven(num: number): boolean;
 export function timeFromDate(date: any): any;
-export function daysBetweenDates(dateA: Date, dateB: Date): Integer;
+export function daysBetweenDates(dateA: Date, dateB: Date): number;
 export function HTMLize(str: string): string;
-export function factorialOfNumber(number: any): any;
+export function factorialOfNumber(number: number): number;
 export function sumOfNumbers(...array: any[]): any;
 export function isObjectEmpty(objectName: any): boolean;
 //# sourceMappingURL=phlib.d.ts.map
