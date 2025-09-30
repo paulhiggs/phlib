dd25ac4bd02d84ecf522d780d2d82d7d7a952afc
Paul Higgs <paul_higgs@hotmail.com>
2025-09-30 06:25:51 +0100

    add ts headers

---
+++ phlib.d.ts
@@ -0,0 +1,27 @@
+/**
+ * return the type of the argument passed
+ * @param {any} arg the argument whose type we are interested in
+ * @param {String} requiredType  the desired tyoe
+ * @returns {boolean or string} the type of the argument or a boolean if the type matches the requiredType
+ */
+export function datatypeIs(arg: any, requiredType?: string): boolean;
+export function quote(str: any): any;
+export function elementize(str: any): any;
+export function attribute(attr: any, elem?: string): any;
+export function celsiusToFahrenheit(celsius: any): number;
+export function fahrenheitToCelsius(fahrenheit: any): number;
+export function average(...args: any[]): number;
+export function toFixed(n: any, fixed: any): number;
+export function randomNumberInRange(min: number, max: number): number;
+export function randomBoolean(): boolean;
+export function coinToss(): string;
+export function isWeekday(date: Date): boolean;
+export function reverse(str: any): any;
+export function isEven(num: any): boolean;
+export function timeFromDate(date: any): any;
+export function daysBetweenDates(dateA: Date, dateB: Date): Integer;
+export function HTMLize(str: string): string;
+export function factorialOfNumber(number: any): any;
+export function sumOfNumbers(...array: any[]): any;
+export function isObjectEmpty(objectName: any): boolean;
+//# sourceMappingURL=phlib.d.ts.map
